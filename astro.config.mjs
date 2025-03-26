4eb2a80896b386385ef4b8b0869f5507dff2f2e2
Emanuel Suriano <emanuel.suriano@revolut.com>
2025-03-26 12:20:12 +0100

    add new phrase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://emasuriano.github.io",
   base: "ollama-ci-demo",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
